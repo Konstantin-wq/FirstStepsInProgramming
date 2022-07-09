83b598805e3488b1ea20ed830e29e23488d2e0b7
Konstantin <kv_psk@mail.ru>
2022-07-09 16:29:35 +0300

    Add SolutionTask7

---
+++ SolutionTask7/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая на вход принимает число и выдает,является ли оно четным
+string? inputA = Console.ReadLine(); 
+
+if (inputA !=null)
+{
+     int inputNumberOne = int.Parse(inputA); 
+     int div = inputNumberOne%10;
+     if(div == 0)
+     {
+        Console.WriteLine("Число "+ inputNumberOne + " четное");
+     }
+    else
+    {
+        Console.WriteLine("Число "+ inputNumberOne + " нечетное");
+    }
+}   
