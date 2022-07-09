1f058d28460bb26f79316e1df31f445abb27f17a
Konstantin <kv_psk@mail.ru>
2022-07-09 16:17:57 +0300

    Add SolutionTask6

---
+++ SolutionTask6/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая принимает на вход три числа, и выдает максимальное из этих чисел
+string? inputA = Console.ReadLine(); 
+string? inputB = Console.ReadLine(); 
+string? inputС = Console.ReadLine(); 
+
+if (inputA !=null && inputB !=null && inputС !=null)
+{
+     int inputNumberOne = int.Parse(inputA); 
+     int inputNumberTwo = int.Parse(inputB); 
+     int inputNumberThree = int.Parse(inputС);
+
+if(inputNumberOne > inputNumberTwo && inputNumberOne > inputNumberThree)
+{
+    Console.WriteLine("Максимальное число " + inputNumberOne);
+}
+if(inputNumberTwo > inputNumberOne && inputNumberTwo > inputNumberThree)
+{
+    Console.WriteLine("Максимальное число " + inputNumberTwo);
+}
+if(inputNumberThree > inputNumberOne && inputNumberThree > inputNumberTwo)
+{
+    Console.WriteLine("Максимальное число " + inputNumberThree);
+}
+}
+
+
+
+
+
+
+
