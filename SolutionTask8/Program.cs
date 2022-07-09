090534fca1b87bca8a4a4ddcf5f3b0071f3b7781
Konstantin <kv_psk@mail.ru>
2022-07-09 17:11:18 +0300

    Add SolutionTask8

---
+++ SolutionTask8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа, которая на вход принимает число N, а на выходе показывает все чётные числа от 1 до N
+string? inputA = Console.ReadLine(); 
+
+if (inputA !=null)
+{
+     int inputNumberOne = int.Parse(inputA);
+     
+     int numberB = 2;
+     
+     string lineOutput = "";
+
+     while (inputNumberOne > numberB)
+     {
+        lineOutput = lineOutput + numberB +", ";
+        numberB = numberB + 2;
+     }
+     Console.WriteLine(lineOutput);
+}
