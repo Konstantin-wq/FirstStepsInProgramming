f9e55356f3ecbcc86a7df650c6be674548592da0
Konstantin <kv_psk@mail.ru>
2022-07-09 15:02:28 +0300

    Add SolutionTask5

---
+++ SolutionTask5/Program.cs
@@ -0,0 +1,19 @@
+﻿//Программа, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+string? inputA = Console.ReadLine(); 
+string? inputB = Console.ReadLine(); 
+
+if (inputA !=null && inputB !=null)
+{
+     int inputNumberOne = int.Parse(inputA); 
+     int inputNumberTwo = int.Parse(inputB); 
+if(inputNumberOne > inputNumberTwo)
+{
+    Console.WriteLine("Первое число " + inputNumberOne + " больше второго числа "+inputNumberTwo);
+    Console.WriteLine("Максимальное число " + inputNumberOne);
+}
+else
+{
+    Console.WriteLine("Первое число " + inputNumberOne + " меньше второго числа "+inputNumberTwo);
+    Console.WriteLine("Максимальное число " + inputNumberTwo);
+}
+}
