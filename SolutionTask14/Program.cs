2bebda279735e82d30b464dc5f788dce262a292f
Konstantin <kv_psk@mail.ru>
2022-07-13 17:41:54 +0300

    Added Solution Task 14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,30 @@
+﻿//Программа, которая получает на вход трехзначное число, и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    int a = int.Parse(inputNumber);
+    int count = 0;
+    int numberCheck = a;                
+        while (numberCheck > 0)                 //Проверка разрядности числа
+        {
+            numberCheck = numberCheck / 10;
+            count++;
+        }
+
+if (count == 3)
+    {
+    int newNumberA = a % 100;
+    int newNumberB = newNumberA / 10;
+    Console.WriteLine(newNumberB);
+    }
+else
+    {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Число не подходит по разрядности. Разряд числа  " + count);
+    }
+}
+
+
