14d1e06b4919f1ae65c4e427d7768071a467cb42
Konstantin <kv_psk@mail.ru>
2022-08-04 00:37:40 +0300

    Added SolutionTask30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,39 @@
+﻿//Программа, которая задает массив из 8 элементов и выводит их на экран
+//*Ввод длинны массива и диапозон значений задается с клавиатуры
+
+Console.WriteLine("Введите длину массива: ");
+string? arrayLength = Console.ReadLine();
+
+Console.WriteLine("Введите диапазон значений для массива:");
+
+Console.WriteLine("Введите начальное значение: ");
+string? arrayStart = Console.ReadLine();
+
+Console.WriteLine("Введите конечное значение:");
+string? arrayFinish = Console.ReadLine();
+
+if(arrayLength!=null && arrayStart!=null && arrayFinish!=null)
+{
+
+    int lengthOfArray = int.Parse(arrayLength);
+    int startOfArray = int.Parse(arrayStart);
+    int finishOfArray = int.Parse(arrayFinish);
+    System.Random numberSintezator = new Random();
+
+//Заполняем массив
+void FillingArray()
+{
+    int i = 0;
+    Console.Write("[");
+    while(i<lengthOfArray-1)
+        {
+        Console.Write(numberSintezator.Next(startOfArray,finishOfArray+1)+",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(startOfArray,finishOfArray));
+    Console.Write("]");
+}
+FillingArray();
+
+}
+
