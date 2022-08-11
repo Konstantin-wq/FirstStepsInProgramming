d91b89d5d8b47b703d750abb3f8eb81d2a135163
Konstantin <kv_psk@mail.ru>
2022-08-11 19:48:06 +0300

    Added SolutionTask 45-46

---
+++ SolutionTask45/Program.cs
@@ -0,0 +1,60 @@
+﻿/*--------------------------------------------------------------------------------------------------------------------
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+----------------------------------------------------------------------------------------------------------------------
+*/
+
+Console.WriteLine("Введите количество чисел которые будем проверять: ");
+int m = 0;
+int[] arrayNumbers = new int[m];
+
+//Вводим данные с консоли
+int[] ReadNumber(int m)
+{
+    m = int.Parse(Console.ReadLine() ?? "");
+    int[] ReadNumber = new int[m];
+    int i = 0;
+    
+    while (i < m)
+    {
+        Console.WriteLine("Введите число " + (i+1));
+        ReadNumber[i] = int.Parse(Console.ReadLine() ?? "");
+        i++;
+
+    }
+    
+    return ReadNumber;
+
+
+
+}
+
+//Находим числа, которые больше 0
+int CalculateNumber(int[] arrayA)
+{
+    int count = 0;
+    int i = 0;
+    while (i < arrayA.Length)
+    {
+        if (arrayA[i] > 0)
+        {
+            count++;
+            
+        }
+        i++;
+    }
+    
+    return (count);
+}
+
+//Вывод на экран результатов
+void PrintResult(int count)
+{
+    Console.WriteLine("Количество заданных положительных элементов: " + count);
+
+}
+
+
+
+int[] numbers = ReadNumber(m);
+PrintResult(CalculateNumber(numbers));
---
+++ SolutionTask46/Program.cs
@@ -0,0 +1,68 @@
+﻿/*-----------------------------------------------------------------------------------------------------------------------
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+-------------------------------------------------------------------------------------------------------------------------
+*/
+
+//Объвляем глобальные переменные
+double k_1 = 0;
+double b_1 = 0;
+double k_2 = 0;
+double b_2 = 0;
+double calcX = 0;
+double calcY = 0;
+
+//Считываем необходимые данные с консоли
+void ReadIntegers() 
+{
+    Console.WriteLine("Введите коэффициенты для уравнения y = k1 * x + b1 :");
+    
+    Console.WriteLine("Введите коэффициент k1:");
+    k_1 = double.Parse(Console.ReadLine() ?? "");
+    
+    Console.WriteLine("Введите коэффициент b1: ");
+    b_1 = double.Parse(Console.ReadLine() ?? "");
+
+    Console.WriteLine("Введите коэффициенты для уравнения y = k2 * x + b2 :");
+    
+    Console.WriteLine("Введите коэффициент k2:");
+    k_2 = double.Parse(Console.ReadLine() ?? "");
+    
+    Console.WriteLine("Введите коэффициент b2:");
+    b_2 = double.Parse(Console.ReadLine() ?? "");
+    
+
+}
+
+//Рассчитываем координаты точки пересечения
+void CalculateCoordOfPoint(double k1, double b1, double k2, double b2)
+{
+
+calcX = (b2-b1)/(k1-k2);
+
+calcY = k1*calcX+b1;
+
+}
+
+//Выводим на экран координаты точки
+void PrintCoordinateOfPoint(double numX , double numY )
+{
+Console.WriteLine("Коордтнаты точки пересечения: ");
+Console.WriteLine("Координата X: " + calcX);
+Console.WriteLine("Координата Y: " + calcY);
+}
+
+ReadIntegers();
+CalculateCoordOfPoint(k_1, b_1, k_2, b_2);
+PrintCoordinateOfPoint(calcX, calcY);
+
+
+
+
+
+
+
+
+
+
