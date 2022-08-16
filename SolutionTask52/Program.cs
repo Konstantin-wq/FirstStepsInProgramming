ca3f904a3bd633db5fc6c2dab910fad4342c50bb
Konstantin <kv_psk@mail.ru>
2022-08-16 17:46:15 +0300

    Added Solution Task 52-53

---
+++ SolutionTask52/Program.cs
@@ -0,0 +1,182 @@
+﻿/*--------------------------------------------------------------------------------------
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+ и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+* Заполнить числами Фиббоначи и выделить цветом найденную цифру
+----------------------------------------------------------------------------------------
+*/
+
+
+Console.Clear();
+int inputNumberString = 0;
+int inputNumberColumn = 0;
+
+// Метод заполнения двумерного массива числами Фибоначчи
+int[,] FillTwoDimArray(int countRow, int countColumn)
+
+{
+    int amountOfNumbers = countColumn * countRow;
+    int[] fibonacciNumbers = new int[amountOfNumbers];
+
+    if (amountOfNumbers == 2)
+    {
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+    }
+    else if (amountOfNumbers == 1)
+    {
+        fibonacciNumbers[0] = 0;
+    }
+    else
+    {
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+        int s = 0;
+        int f1 = 1;
+        int f2 = 1;
+        for (int k = 2; k < fibonacciNumbers.Length; k++)
+        {
+
+            s = f1 + f2;
+            f1 = f2;
+            f2 = s;
+            fibonacciNumbers[k] = s;
+
+        }
+
+    }
+
+
+    int i = 0; int j = 0;
+
+    int[,] outArray = new int[countRow, countColumn];
+
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = fibonacciNumbers[i * countRow + j];
+            j++;
+        }
+        i++;
+    }
+
+    return outArray;
+}
+
+
+//Выводим массив на экран
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i, j] + "\t");
+            j++;
+        }
+        Console.WriteLine(); //альтернативный способ Console.WriteLine("\n");
+        i++;
+
+    }
+
+}
+
+
+//Читаем число с консоли
+void ReadNumber(int[,] inputArray)
+{
+
+    Console.WriteLine("Введите позицию элемента столбце :");
+    inputNumberString = int.Parse(Console.ReadLine() ?? "");
+    Console.WriteLine("Введите позицию элемента в строке :");
+    inputNumberColumn = int.Parse(Console.ReadLine() ?? "");
+
+
+
+
+
+}
+
+
+//Метод поиска введенного числа
+void SearchNumber(int[,] inputArray)
+{
+    int[,] outArray = inputArray;//new int[inputArray.GetLength(0), inputArray.GetLength(1)];
+    try
+    {
+        for (int i = 0; i < inputArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inputArray.GetLength(1); j++)
+                if (inputArray[i, j] == inputArray[inputNumberString, inputNumberColumn])
+                {
+                    Console.WriteLine("Число по заданным координатам есть в заданном массиве, его значение равно:  " + inputArray[inputNumberString, inputNumberColumn] + " Индекс элемента: [{0},{1}]", i, j);
+                }
+        }
+    }
+    catch
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Введенный индекс в заданном массиве отсутствует");
+        Console.ResetColor();
+    }
+
+
+
+
+
+
+
+}
+
+//Выделяем найденный элемент цветом и выводим на экран
+void ColouringNumber(int[,] inputArray)
+{
+    try
+    {
+        int i = 0; int j = 0;
+
+        while (i < inputArray.GetLength(0))
+        {
+            j = 0;
+            while (j < inputArray.GetLength(1))
+            {
+                if (inputArray[inputNumberString, inputNumberColumn] == inputArray[i, j])
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write(inputArray[i, j] + "\t");
+                Console.ResetColor();
+                j++;
+            }
+            Console.WriteLine(); //альтернативный способ Console.WriteLine("\n");
+            i++;
+
+        }
+    }
+    catch
+    {
+        
+    }
+}
+
+
+
+
+
+
+int[,] twoDimArray = FillTwoDimArray(5, 5);
+PrintTwoDimArray(twoDimArray);
+
+
+ReadNumber(twoDimArray);
+SearchNumber(twoDimArray);
+ColouringNumber(twoDimArray);
+
+
+
+
---
+++ SolutionTask53/Program.cs
@@ -0,0 +1,169 @@
+﻿/*---------------------------------------------------------------------------------------------------------
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+* Дополнительно вывести среднее арифметическое по диагоналям и диагональ выделить разным цветом.
+-------------------------------------------------------------------------------------------------------------
+*/
+
+//Объявлем массив с палитрой цветов
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue, ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen, ConsoleColor.DarkMagenta, ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.Green,
+                                        ConsoleColor.Magenta, ConsoleColor.Red, ConsoleColor.White, ConsoleColor.Yellow};
+
+
+
+
+// Метод заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+
+{
+    System.Random numberSynteztor = new System.Random();
+
+    int i = 0; int j = 0;
+
+    int[,] outArray = new int[countRow, countColumn];
+
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSynteztor.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+
+    return outArray;
+}
+
+
+//Метод вычисления среднего арифметического по столбцам
+double[] CalcAvg(int[,] inputArray)
+{
+
+    double[] average = new double[inputArray.GetLength(0)];
+    double sum = 0;
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            sum = sum + inputArray[j, i];
+            average[i] = sum / inputArray.GetLength(1);
+
+        }
+    }
+    return (average);
+}
+
+
+//Нахождение среднего арифметического по главной диагонали
+double CalcDiagTwoDimArray(int[,] inputArray)
+{
+    double outSumDiagonal = 0;
+    double avg = 0;
+    int k = 0;
+    {
+        int i = 0; int j = 0;
+        while (i < inputArray.GetLength(0))
+        {
+            j = 0;
+            while (j < inputArray.GetLength(1))
+            {
+                if (i == j)
+                {
+                    outSumDiagonal = outSumDiagonal + inputArray[j, i];
+                    k = k + 1;
+
+                }
+                j++;
+            }
+            i++;
+
+        }
+
+    }
+    avg = outSumDiagonal / k;
+
+    return avg;
+}
+
+
+
+//Выод массива на экран
+void PrintRandomColourTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(inputArray[i, j] + "\t");
+            Console.ResetColor();
+            j++;
+        }
+        Console.WriteLine(); //альтернативный способ Console.WriteLine("\n");
+        i++;
+
+    }
+    Console.ResetColor();
+}
+
+
+//Метод печати среднего арифметического значения
+void PrintAvgSum(double[] inputArray)
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + "\t");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+
+//Выводим на экран главную диагональ
+void PrintMainDiag(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            if(i == j)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            }
+            Console.Write(inputArray[i, j] + "\t");
+            Console.ResetColor();
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+
+    }
+    Console.ResetColor();
+}
+
+
+int[,] twoDimArray = FillTwoDimArray(4, 4);
+PrintRandomColourTwoDimArray(twoDimArray);
+
+double[] averageSum = CalcAvg(twoDimArray);
+PrintAvgSum(averageSum);
+
+
+double sum = CalcDiagTwoDimArray(twoDimArray);
+Console.WriteLine("Среднее арифметическое элементов главной диагонали: " + sum);
+
+int[,] twoDimArrayMainDiag = twoDimArray;
+PrintMainDiag(twoDimArray);
