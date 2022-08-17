d1f2c0b0061e4d0bceec2d03072dcaf23fd03ac8
Konstantin <kv_psk@mail.ru>
2022-08-17 14:42:53 +0300

    Added SolutionTask60

---
+++ SolutionTask60/Program.cs
@@ -0,0 +1,81 @@
+﻿/*---------------------------------------------------------------------------
+Задайте прямоугольный двумерный массив.Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов
+-----------------------------------------------------------------------------
+*/
+
+// Метод заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+
+{
+    System.Random numberSynteztor = new System.Random();
+
+    int i = 0; int j = 0;
+
+    int[,] outArray = new int[countRow, countColumn];
+
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSynteztor.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+
+    return outArray;
+}
+
+
+//Вывод массива на экран
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i, j] + "\t ");
+            j++;
+        }
+        Console.WriteLine(); //альтернативный способ Console.WriteLine("\n");
+        i++;
+
+    }
+
+}
+
+int FindRow(int[,] inputArray)
+{
+    int i = 0; int j = 0;  int minValue = int.MaxValue; int indexRow = 0;
+
+    for (i = 0; i < inputArray.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (j = 0; j < inputArray.GetLength(1); j++)
+        
+            sum += inputArray[i, j];
+
+            if (sum < minValue)
+            {
+                minValue = sum;
+                indexRow= i;
+            }
+        
+
+    }
+    return indexRow;
+}
+
+
+
+int[,] twoDimArray = FillTwoDimArray(4, 3);
+Console.WriteLine("Изначальный массив:");
+PrintTwoDimArray(twoDimArray);
+
+int ind = FindRow(twoDimArray);
+
+Console.WriteLine("Миниимальная сумма элементов в строке " + (ind+1) +"." + " Индекс строки = " + "["+ ind+"]");
