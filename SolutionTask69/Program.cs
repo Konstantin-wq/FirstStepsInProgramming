9e90cb5ecebb17600ea5b8df96f698dc9298b5fb
Konstantin <kv_psk@mail.ru>
2022-08-20 12:38:28 +0300

    Added Solution Task69

---
+++ SolutionTask69/Program.cs
@@ -0,0 +1,48 @@
+﻿/*----------------------------------------------------------------------------------------------
+Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов
+в промежутке от M до N
+------------------------------------------------------------------------------------------------
+*/
+
+int numberM = ReadData("Задайте число M:");
+int numberN = ReadData("Задайте число N:");
+
+
+if (numberM < numberN)
+{
+
+    SumNaturalNumber(numberM, numberN, 0);
+}
+else
+{
+
+    SumNaturalNumber(numberN, numberM, 0);
+}
+
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+
+//Рекурсивный метод нахождения суммы 
+void SumNaturalNumber(int m, int n, int sum)
+{
+
+    if (m > n)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + m;
+    SumNaturalNumber(m + 1, n, sum);
+
+}
+
+
+
+
+
