60897401237fc8839f0b3c1dc4b2ad3a62aeb3ac
Konstantin <kv_psk@mail.ru>
2022-08-20 11:21:44 +0300

    Added Solution Task68

---
+++ SolutionTask68/Program.cs
@@ -0,0 +1,41 @@
+﻿/*----------------------------------------------------------------------------------------------
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа от M до N
+
+-------------------------------------------------------------------------------------------------
+*/
+
+int numberM = ReadData("Задайте число M:");
+int numberN = ReadData("Задайте число N:");
+
+
+if (numberM < numberN)
+{
+    NaturalNumberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNumberPrinter(numberN, numberM);
+}
+
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+
+
+void NaturalNumberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;
+    Console.Write(m + " ");
+    NaturalNumberPrinter(m + 1, n);
+
+}
+
+
+
+
+
